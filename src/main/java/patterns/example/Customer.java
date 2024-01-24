5b127d0746e686c4e3da5b182d0dc21c5dffd63c
Kirill Belyaev <kirillbelyaev2921@gmail.com>
2024-01-24 11:04:40 +0200

    Move logic from Customer to Rental class.

--- src/main/java/patterns/example/Customer.java
+++ src/main/java/patterns/example/Customer.java
@@ -24,26 +24,11 @@ class Customer {
         int frequentRenterPoints = 0;
         String result = "Rental Record for " + getName() + "\n";
         for (Rental each : rentals) {
-            double thisAmount = 0;
-            //determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case REGULAR -> {
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                }
-                case NEW_RELEASE -> thisAmount += each.getDaysRented() * 3;
-                case CHILDRENS -> {
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                }
-            }
+            double thisAmount = each.getPriceForMovie();
             // add frequent renter points
             frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints++;
+            frequentRenterPoints += each.getFrequentRenterPoint();
+
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n";
             totalAmount += thisAmount;
--- src/main/java/patterns/example/Rental.java
+++ src/main/java/patterns/example/Rental.java
@@ -1,5 +1,7 @@
 package patterns.example;
 
+import static patterns.example.Movie.MovieType.NEW_RELEASE;
+
 class Rental {
     private final Movie movie;
     private final int daysRented;
@@ -15,5 +17,29 @@ class Rental {
 
     public Movie getMovie() {
         return movie;
+    }
+
+    public double getPriceForMovie() {
+        double result = 0;
+        switch (movie.getPriceCode()) {
+            case REGULAR -> {
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+            }
+            case NEW_RELEASE -> result += daysRented * 3;
+            case CHILDRENS -> {
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+            }
+        }
+        return result;
+    }
+
+    public int getFrequentRenterPoint() {
+        return (movie.getPriceCode() == NEW_RELEASE)
+                && (daysRented > 1)
+                ? 1 : 0;
     }
 }
